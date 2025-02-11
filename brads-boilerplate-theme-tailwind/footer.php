081fe03f8cf8bafdb22dc8a13b3468e697f9cde0
Brad Schiff <LearnWebCode@users.noreply.github.com>
2025-02-11 13:23:26 -0800

    Update footer.php

--- brads-boilerplate-theme-tailwind/footer.php
+++ brads-boilerplate-theme-tailwind/footer.php
@@ -1,4 +1,4 @@
-<div class="border-t mt-4 py-4">
+<div class="border-t border-gray-300 mt-4 py-4">
   <div class="max-w-4xl px-4 mx-auto text-xs text-gray-400">Your footer content here.</div>
 </div>
 
